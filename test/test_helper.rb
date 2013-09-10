d66f8a31cd6b674df005f2a591cebb21cf14bc47
denenbergt <timothydenenberg@Timothys-MacBook-Air.local>
2013-09-09 20:57:22 -0500

    Finish static pages

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,4 +12,9 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+Rspec.configure do |config|
+  config.include Capybara::DSL
+end
+
 end
